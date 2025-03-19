53c1383a62ab861fc1001d071bcf7f6e0f4e7093
daniel <gstlobao@gmail.com>
2025-03-19 18:52:14 -0300

    conta

--- trilahajava/src/App.java
+++ trilahajava/src/App.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        
+
+        String nome , agencia , conta;
+        
+        
+        String resultadonome ="Mario Andrade";
+        int resultadoconta = 1021;  
+        String resultadoAgencia ="067-8";
+        double saldo = 237.48;
+        
+
+        Scanner scanner = new Scanner(System.in); // Inicialize o Scanner
+
+        System.out.print("Digite Numero Da Sua Agencia: ");
+        agencia = scanner.nextLine();
+
+        
+
+        System.out.print("Digite Numero Da Sua Conta: ");
+        conta = scanner.nextLine();
+       
+
+        int contaInteiro = Integer.parseInt(conta); // Converte a string 'conta' para inteiro
+      
+
+        if (agencia.equals(resultadoAgencia)&& contaInteiro==resultadoconta){
+
+            System.out.println(" Olá "+resultadonome+" Obrigado por criar uma conta em nosso banco sua agencia é "+agencia +" conta"+resultadoconta+" e seu saldo é  "+saldo);
+        }
+        else{
+
+            System.out.println("digite os dados corretamnte");
+        }
+       
+
+       
+        
+
+
+        
+
+
+        
+        
+       
+        
+
+
+
     }
 }
